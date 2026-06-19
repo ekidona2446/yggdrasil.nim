55c35cce09d08b7aa29bdf05ddf60c39e47f92a3
ekidona2446 <hentaisloli@gmail.com>
2026-06-19 15:20:15 +0300

    Delete tests directory

--- tests/test_monocypher_backend.nim
+++
@@ -1,7 +0,0 @@
-import std/unittest
-import ../src/crypto/monocypher
-
-suite "monocypher optional backend":
-  test "runtime capability probe does not crash":
-    discard available()
-    check true
